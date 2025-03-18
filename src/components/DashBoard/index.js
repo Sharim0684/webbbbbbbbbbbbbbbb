a39434f274c730d4d4b3761b1fe0b85ad248e6bf
Sathish-241 <ksathish241@gmail.com>
2025-03-18 11:59:39 +0530

    mobile view in dashboard

--- src/components/DashBoard/index.js
+++ src/components/DashBoard/index.js
@@ -15,17 +15,16 @@ const DashBoard=()=>{
                 <Header/>
             </Box>
             <Container maxWidth="xl" >            
-            <Stack direction={{xs:"column", md:"row"}} justifyContent="center" alignItems='center'  spacing={2} style={{border:'',height:'auto',}}>
-            <Box alignSelf='flex-start' sx={{flex:1,border:'',textAlign:'start'}}>
-                <Typography variant='h3' sx={{fontFamily:'Poppins',color:'#561f5b',marginLeft:'100px',marginTop:'100px',textAlign:"center"}}>Hello User</Typography>
+            <Stack direction={{xs:"column", md:"row"}} justifyContent="center" alignItems='center'  spacing={2} sx={{border:'',height:'auto',marginTop:'10px'}}>
+            <Box alignSelf='flex-start' sx={{flex:1,border:'',width:'100%',textAlign:'start'}}>
+                <Typography variant='h3' sx={{fontFamily:'Poppins',color:'#561f5b',marginLeft:'',marginTop:'50px',textAlign:"center"}}>Hello User</Typography>
             </Box>
-            <Box sx={{flex:2,height:"500px",width:"auto",border:'',display:'flex',flexDirection:"column",justifyContent:'center',alignItems:'center'}}>
+            <Box  sx={{flex:2,height:"500px",width:"auto",border:'',display:'flex',flexDirection:"column",justifyContent:'center',alignItems:'center',marginTop:"20px"}}>
                     <img src={MediaManagerImg} alt='MediaMangerImg' style={{height:'350px',width:"350px",backgroundColor:'transparent'}} />
                     <Typography variant='h5'sx={{textAlign:'',fontFamily:'poppins',color:'#561f5b'}}>Welcome to  Webapp</Typography>
                     <Typography variant='' sx={{fontFamily:'Poppins', color:'blue'}}>Thank you for using Webapp</Typography>
                     <Typography variant='' sx={{fontFamily:'Poppins',color:'#561f5b',marginBottom:'10px'}}>Smart solution to share block post.</Typography>
                     <Button sx={{marginTop:""}}><Link style={{textDecoration:'none',color:'white'}} to="/add-accounts" >Accounts</Link></Button>
-
             </Box>
             </Stack>
         </Container>
